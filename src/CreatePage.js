d33fa143b9424c06c06d3ed2d3a877b17cacaa20
cliffmax85 <cliffmax85@gmail.com>
2022-02-10 09:54:03 -0800

    added list page

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,8 +1,16 @@
 import { useState } from 'react';
 import { createGame } from './services/fetch-utils';
+import { useHistory } from 'react-router-dom';
 
 export default function CreatePage() {
   // you'll need the history hook from react-router-dom to do your redirecting in the handleSubmit
+  const history = useHistory();
+  const [title, setTitle] = useState('');
+  const [genre, setGenre] = useState('');
+  const [designer, setDesigner] = useState('');
+  const [description, setDiscription] = useState('');
+  const [minPlayers, setMinPlayers] = useState('');
+  const [maxPlayers, setMaxPlayers] = useState('');
 
   // here's the state you'll need:
     // title;
@@ -16,6 +24,15 @@ export default function CreatePage() {
     e.preventDefault();
 
     // create a game
+    await createGame({
+      title,
+      genre,
+      designer,
+      description,
+      min_players: minPlayers,
+      max_players: maxPlayers,
+    });
+    history.push('/board-games');
 
     // use history.push to send the user to the list page
   }
